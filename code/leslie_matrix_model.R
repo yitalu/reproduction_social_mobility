73c80f78cdb8f15420926020cd265dda30aeb4c0
Yi-Ta Lu <yi_ta_lu@eva.mpg.de>
2022-03-11 11:58:38 +0100

    add data and figures

--- code/leslie_matrix_model.R
+++ code/leslie_matrix_model.R
@@ -45,9 +45,9 @@ population_total <- population_data[, num_class + 1]
 
 # Plot Population Over Time ----
 # Uncomment any of these lines if you want to save the figures:
-# png(file="./matrix_models/figures/population_over_time.png", width = 1200, height = 800, res = 180)
-# pdf(file="./matrix_models/figures/population_over_time.pdf", width = 6, height = 4.3)
-# tiff(file="./matrix_models/figures/population_over_time.tiff", width = 2400, height = 1600, res = 300)
+# png(file="./figures/population_over_time.png", width = 1200, height = 800, res = 180)
+# pdf(file="./figures/population_over_time.pdf", width = 6, height = 4.3)
+# tiff(file="./figures/population_over_time.tiff", width = 2400, height = 1600, res = 300)
 
 plot(population_data[, num_class + 1], type = "l", lty = "solid", lwd = 1, xlab = "Time", ylab = "Population", main = "Population over time")
 
@@ -75,11 +75,11 @@ for (t in 1:(generation - 1)) {
 }
 
 # plot growth rate over time
-# png(file="./matrix_models/figures/growth_rate_over_time.png", width = 1200, height = 800, res = 180)
-# pdf(file="./matrix_models/figures/growth_rate_over_time.pdf", width = 6, height = 4.3)
-# tiff(file="./matrix_models/figures/growth_rate_over_time.tiff", width = 2400, height = 1600, res = 300)
+# png(file="./figures/growth_rate_over_time.png", width = 1200, height = 800, res = 180)
+# pdf(file="./figures/growth_rate_over_time.pdf", width = 6, height = 4.3)
+# tiff(file="./figures/growth_rate_over_time.tiff", width = 2400, height = 1600, res = 300)
 # plot(growth_rate_instant[, num_class + 1], type = "l", lty="solid", lwd=1, xlab = "Time", ylab = "Growth Rate", main = "Growth Rate over Time", ylim = c(3, 5.5))
-plot(growth_rate_instant[, num_class + 1], type = "l", lty="solid", lwd=1, xlab = "Time", ylab = "Growth Rate", main = "Growth Rate over Time", ylim = c(2, 10))
+plot(growth_rate_instant[, num_class + 1], type = "l", lty="solid", lwd=1, xlab = "Time", ylab = "Growth Rate", main = "Growth Rate over Time", ylim = c(2, 6))
 
 # lines(growth_rate_instant[, 1], type="l", lty="dashed")
 # lines(growth_rate_instant[, 2], type="l", lty="dotted")
@@ -89,7 +89,7 @@ for (i in 1:num_class) {
 }
 
 abline(h = growth_rate_eigenvalue, lty = 2, col="red")
-legend(6.5, 5.5, legend = c("Total", "Lower Class", "Middle Class", "Upper Class", "Eigenvalue"), lty = c(1:4, 2), lwd = c(1, 1, 1, 1, 1), col = c(rep("black", 4), "red"), cex = 0.8)
+legend(6.5, 6, legend = c("Total", "Lower Class", "Middle Class", "Upper Class", "Eigenvalue"), lty = c(1:4, 2), lwd = c(1, 1, 1, 1, 1), col = c(rep("black", 4), "red"), cex = 0.8)
 # dev.off()
 
 
@@ -112,16 +112,15 @@ group <- rep(c("Lower Class", "Middle Class", "Upper Class"), times = generation
 group <- rep(as.character(c(1:num_class)), times = generation)
 data <- data.frame(time, value, group)
 
-# png(file="./matrix_models/figures/proportion_over_time.png", width = 1200, height = 800, res = 180)
-# pdf(file="./matrix_models/figures/proportion_over_time.pdf", width = 6, height = 4.3)
-# tiff(file="./matrix_models/figures/proportion_over_time.tiff", width = 2400, height = 1600, res = 300)
+# png(file="./figures/proportion_over_time.png", width = 1200, height = 800, res = 180)
+# pdf(file="./figures/proportion_over_time.pdf", width = 6, height = 4.3)
+# tiff(file="./figures/proportion_over_time.tiff", width = 2400, height = 1600, res = 300)
 ggplot(data, aes(x = time, y = value, fill = group)) + 
   geom_area(alpha = 0.6) + 
   geom_hline(yintercept = c(proportion_eigenvector[3], (1-proportion_eigenvector[1])), col = c("black", "black"), lty = c(2, 2), lwd = c(0.3, 0.3)) + 
   ggtitle("Proportion over Time") + 
   labs(title = "Age Class Proportions over Time", x = "Time", y = "Proportion") + 
   theme(plot.title = element_text(hjust = 0.5))
-
 
 # dev.off()
 
@@ -161,9 +160,9 @@ group <- rep(c("Lower Class", "Middle Class", "Upper Class"), times = generation
 group <- rep(as.character(c(1:num_class)), times = generation)
 data <- data.frame(time, value, group)
 
-# png(file="./matrix_models/figures/fitness_over_time.png", width = 1200, height = 800, res = 180)
-# pdf(file="./matrix_models/figures/fitness_over_time.pdf", width = 6, height = 4.3)
-# tiff(file="./matrix_models/figures/fitness_over_time.tiff", width = 2400, height = 1600, res = 300)
+# png(file="./figures/fitness_over_time.png", width = 1200, height = 800, res = 180)
+# pdf(file="./figures/fitness_over_time.pdf", width = 6, height = 4.3)
+# tiff(file="./figures/fitness_over_time.tiff", width = 2400, height = 1600, res = 300)
 ggplot(data, aes(x = time, y = value, fill = group)) + 
   geom_area(alpha = 0.6) + 
   geom_hline(yintercept = c(fitness_eigenvector_normalized[3], (1 - fitness_eigenvector_normalized[1])), col = c("black", "black"), lty = c(2, 2), lwd = c(0.3, 0.3)) + 
